0b579f5a2b9bc8c46b2a5b6067e87ff86fc69630
alexprimak24 <aprimak43@gmail.com>
2025-02-14 20:43:05 +0200

    continued with tests

--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
 from .models import Airport, Flight, Passenger
 
@@ -40,3 +40,41 @@ class FlightTestCase(TestCase):
         a2 = Airport.objects.get(code="BBB")
         f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
         self.assertFalse(f.is_valid_flight())
+    
+    def test_index(self):
+        # Created a client
+        c = Client()
+        # I receive a response from that page and store it in response
+        response = c.get("/flights/")
+        # I check if the page works
+        self.assertEqual(response.status_code, 200)
+        # And amount of flights is 3
+        # response.context["flights"] - this gives whatever was passed as flights
+        self.assertEqual(response.context["flights"].count(),3)
+
+    def test_valid_flight_page(self):
+        a1 = Airport.objects.get(code="AAA")
+        f = Flight.objects.get(origin = a1, destination = a1)
+
+        c = Client()
+        response = c.get(f"/flights/{f.id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_flight_page(self):
+        # "id__max" - returns maximal Id connected that exist in our database
+        max_id = Flight.objects.all().aggregate(max("id"))["id__max"]
+
+        c = Client()
+        response = c.get(f"/flights/{max_id + 1}")
+        # so we try to access the page which index is +1 of the max 
+        self.assertEqual(response.status_code,404)
+
+    def test_flight_page_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice",last="Adams")
+        f.passengers.add(p)
+
+        c = Client()
+        response = c.get(f"/flights/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
